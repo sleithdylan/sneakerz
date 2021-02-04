e4e3a6841c16f5a42489275f7de5a07508aab7b4
Dylan Sleith <sleithdylan@gmail.com>
2021-02-04 15:53:00 +0000

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 // Imports Container, Row, Col Component from React Bootstrap
 import { Container, Row, Col } from 'react-bootstrap';
+// Import React Bootstrap Icons
+import * as Icon from 'react-bootstrap-icons';
 
 export class Footer extends Component {
   render() {
@@ -9,8 +11,39 @@ export class Footer extends Component {
       <footer>
         <Container>
           <Row>
-            <Col className="text-center mb-5">
-              &copy; Dylan Sleith | G00354557{' '}
+            <Col className="text-left mb-5">
+              &copy; {new Date().getFullYear()} by Dylan Sleith. All rights
+              reserved.
+            </Col>
+            <Col className="text-right mb-5">
+              <a
+                href="https://github.com/sleithdylan/"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                <Icon.Github className="ml-4 text-dark" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/dylansleith/"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                <Icon.Linkedin className="ml-4 text-dark" />
+              </a>
+              <a
+                href="https://www.dylansleith.com/"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                <Icon.Globe className="ml-4 text-dark" />
+              </a>
+              <a
+                href="mailto:sleithdylan@gmail.com"
+                target="_blank"
+                rel="nofollow noopener noreferrer"
+              >
+                <Icon.Envelope className="ml-4 text-dark" />
+              </a>
             </Col>
           </Row>
         </Container>
